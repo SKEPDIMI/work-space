184091b059ebc0a9ca3c8629d88bb384c0adb498
Tyler J. Diaz <me@tylerdiaz.com>
2018-06-02 18:59:29 -0400

    Cleanup popular spaces

--- src/views/PopularSpaces.js
+++ src/views/PopularSpaces.js
@@ -4,15 +4,14 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 class PopularSpaces extends Component {
-render(){
-return(
-<div>
-<Header/>
-
-<Footer/>
-</div>
-);
-}
+  render(){
+    return(
+      <div>
+        <Header/>
+        <Footer/>
+      </div>
+    );
+  }
 };
 
 export default PopularSpaces;
