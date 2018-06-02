f2813da8e9f5c7fbed3a2789b3a7d616584a9389
Tyler J. Diaz <me@tylerdiaz.com>
2018-06-02 18:57:25 -0400

    Reformat Loading.js

--- src/views/Loading.js
+++ src/views/Loading.js
@@ -2,18 +2,18 @@ import React, {Component} from 'react';
 import Logo from '../assets/logo.png';
 
 class Loading extends Component {
-render(){
-return(
-<div className="loading-screen">
-  <div className="inner-wrapper">
-  <img src={Logo}/>
-  <h1>Work Space</h1>
-  <hr/>
-  <span>Is Now Loading</span>
-  </div>
-</div>
-);
-}
+  render(){
+    return(
+      <div className="loading-screen">
+        <div className="inner-wrapper">
+          <img src={Logo}/>
+          <h1>Work Space</h1>
+          <hr/>
+          <span>Is Now Loading</span>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Loading;
