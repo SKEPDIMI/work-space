139b78604067d6a78047a26977691eb42fc9a7d6
Tyler J. Diaz <me@tylerdiaz.com>
2018-06-02 18:53:49 -0400

    Formatting on Signup

--- src/forms/SignUp.js
+++ src/forms/SignUp.js
@@ -29,17 +29,17 @@ class SignIn extends Component {
     };
 
     $(".form-modal").addClass("success").text("Creating Account...")
-    
-    axios.post(config.apiURL+'/api/users', data)
-    .then(response=>{
-      $(".form-modal").addClass('success').text('Signed in successfully!');
 
-      localStorage.setItem('workspaceToken', JSON.stringify({token:response.data.token}));
-      window.location = '/';
-    })
-    .catch(error=>{
-      $(".form-modal").addClass('failure').text(error.response.data.message || 'Failed to log in!');
-    })
+    axios.post(config.apiURL+'/api/users', data)
+      .then(response=>{
+        $(".form-modal").addClass('success').text('Signed in successfully!');
+
+        localStorage.setItem('workspaceToken', JSON.stringify({token:response.data.token}));
+        window.location = '/';
+      })
+      .catch(error=>{
+        $(".form-modal").addClass('failure').text(error.response.data.message || 'Failed to log in!');
+      })
 
   }
   constructor(props){
@@ -47,43 +47,43 @@ class SignIn extends Component {
     this.state = {};
     this.formSubmit = this.formSubmit.bind(this);
   };
-render(){
-return(
-<div>
-<Header/>
-<Sidemenu/>
-<div className="container wrapper">
-  <div className="row"> 
-    <div className="col">
-      <h1>Welcome to WorkSpace!</h1>
-      <hr/>
-      <span className="form-modal">Logged in!</span>
-    </div>
+  render(){
+    return(
+      <div>
+        <Header/>
+        <Sidemenu/>
+        <div className="container wrapper">
+          <div className="row">
+            <div className="col">
+              <h1>Welcome to WorkSpace!</h1>
+              <hr/>
+              <span className="form-modal">Logged in!</span>
+            </div>
 
-    <div className="col-8">
-      <form onSubmit={this.formSubmit}>
-      <div className="form-group">
-      <label>Username</label>
-      <input name="username" className="form-control" type="text" placeholder="Public username" required/>
-      </div>
-      <div className="form-group">
-      <label>Email</label>
-      <input name="email" className="form-control" type="email" placeholder="Email" required/>
-      </div>
-      <div className="form-group">
-      <label>Password</label>
-      <input name="password" className="form-control" type="password" placeholder="Password" required/>
-      </div>
+            <div className="col-8">
+              <form onSubmit={this.formSubmit}>
+                <div className="form-group">
+                  <label>Username</label>
+                  <input name="username" className="form-control" type="text" placeholder="Public username" required/>
+                </div>
+                <div className="form-group">
+                  <label>Email</label>
+                  <input name="email" className="form-control" type="email" placeholder="Email" required/>
+                </div>
+                <div className="form-group">
+                  <label>Password</label>
+                  <input name="password" className="form-control" type="password" placeholder="Password" required/>
+                </div>
 
-      <button type="submit" className="btn btn-info">Sign Up</button>
-      </form>
-    </div>
-  </div>
-</div>
-<Footer/>
-</div>
-);
-}
+                <button type="submit" className="btn btn-info">Sign Up</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    );
+  }
 };
 
 export default SignIn;
