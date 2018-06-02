f6147b07a555d1ec02303cf4e30d0e0aff5c1bce
Tyler J. Diaz <me@tylerdiaz.com>
2018-06-02 18:52:55 -0400

    Clean up routes file

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -6,31 +6,24 @@ import PopularSpaces from '../views/PopularSpaces';
 import LogIn from '../forms/LogIn';
 import SignUp from '../forms/SignUp';
 import Me from '../views/Me';
-import Loading from '../views/Loading';
 import LogOut from '../views/LogOut';
 
-import store from '../redux/store';
-import SignIn from '../forms/SignUp';
-
 class Routes extends Component {
-render(){
-return(
-  <BrowserRouter>
-  <Switch>
-    <Route exact path="/" render={() => (
-      <Redirect to="/landing"/>
-    )}/>
-    <Route path="/landing" component={Landing}/>
-    <Route path="/popular/spaces" component={PopularSpaces}/>
-    <Route path="/login" component={LogIn}/>
-    <Route path="/signup" component={SignUp}/>
-    <Route path="/logout" component={LogOut}/>
-    <Route path="/me" component={Me} />
-
-  </Switch>
-  </BrowserRouter>
-);
-}
+  render(){
+    return(
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => ( <Redirect to="/landing"/> )}/>
+          <Route path="/landing" component={Landing}/>
+          <Route path="/popular/spaces" component={PopularSpaces}/>
+          <Route path="/login" component={LogIn}/>
+          <Route path="/signup" component={SignUp}/>
+          <Route path="/logout" component={LogOut}/>
+          <Route path="/me" component={Me} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 };
 
 export default Routes;
