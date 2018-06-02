7f9881860de50578d558dbb1396d46b6aa54333d
Tyler J. Diaz <me@tylerdiaz.com>
2018-06-02 18:58:01 -0400

    Reformat Me.js
    This also includes moving some functions to the top of the class definition to
    facilitate following component flow

--- src/views/Me.js
+++ src/views/Me.js
@@ -14,33 +14,6 @@ import avatar from '../assets/avatar.png';
 import store from '../redux/store';
 
 class Me extends Component {
-  onEnabled(){
-    $("form button.btn-info").prop("disabled",false);
-  }
-  formSubmit(event){
-    event.preventDefault();
-    let form = event.target;
-    let elements = form.elements;
-    
-    let data = {};
-    for (let i = 0; i < elements.length; i++) {
-      let currentElement = elements[i];
-      if (currentElement.type != 'submit'){
-        data[currentElement.name] = currentElement.value;
-      }
-    };
-
-    $(".form-modal").addClass("success").text("Saving changes...")
-    
-    axios.put(config.apiURL+'/api/users', data)
-    .then(response=>{
-      $(".form-modal").addClass('failure').text('Changes have been saved.');
-      $("form button.btn-info").prop("disabled",true);
-    })
-    .catch(error=>{
-      $(".form-modal").addClass('failure').text(error.response.data.message || 'Failed to save changes!');
-    });
-  }
   constructor(props){
     super(props);
     this.state = {user:store.getState()};
@@ -55,54 +28,82 @@ class Me extends Component {
       user ? this.setState({user}) : this.setState({user:undefined});
     });
   }
-render(){
-  {
-    if (this.state.user === 'pending') {
-      return(<Loading/>)
-    } else if (this.state.user === undefined) {
-      return(<div><Header/><Sidemenu/>
-      <div className="wrapper">
-          <h1>You are not logged in</h1>
-          <a href="/">Back home</a>
-      </div>
-        <Footer/></div>)
-    } else {
-      return(
-        <div>
-        <Header/><Sidemenu/>
-        <div className="account-dashboard">
-          <header>
-            <img src={avatar}/> <p>{this.state.user.username}</p>
-          </header>
-          <form onSubmit={this.formSubmit} onChange={this.onEnabled} className="container">
-          <p className="form-modal"></p>
-          <div className="form-group">
-            <label>Email</label>
-            <input name="email" className="form-control" type="email" value={this.state.user.email} required readonly/>
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input name="password" className="form-control" type="password" required/>
-          </div>
-          <hr/>
-            <div className="form-group">
-              <label>User name</label>
-              <input name="newUsername" className="form-control" defaultValue={this.state.user.username}/>
-            </div>
-            <div className="form-group">
-              <label>Bio</label>
-              <textarea name="bio" className="form-control" defaultValue={this.state.user.bio}></textarea>
-            </div>
 
-            <button className="btn btn-info" type="submit" disabled>Update</button>
-          </form>
-        </div>
-        <Footer/>
-        </div>
-      )
+  onEnabled(){
+    $("form button.btn-info").prop("disabled",false);
+  }
+  formSubmit(event){
+    event.preventDefault();
+    let form = event.target;
+    let elements = form.elements;
+
+    let data = {};
+    for (let i = 0; i < elements.length; i++) {
+      let currentElement = elements[i];
+      if (currentElement.type != 'submit'){
+        data[currentElement.name] = currentElement.value;
+      }
+    };
+
+    $(".form-modal").addClass("success").text("Saving changes...")
+
+    axios.put(config.apiURL+'/api/users', data)
+      .then(response=>{
+        $(".form-modal").addClass('failure').text('Changes have been saved.');
+        $("form button.btn-info").prop("disabled",true);
+      })
+      .catch(error=>{
+        $(".form-modal").addClass('failure').text(error.response.data.message || 'Failed to save changes!');
+      });
+  }
+  render(){
+    {
+      if (this.state.user === 'pending') {
+        return(<Loading/>)
+      } else if (this.state.user === undefined) {
+        return(<div><Header/><Sidemenu/>
+          <div className="wrapper">
+            <h1>You are not logged in</h1>
+            <a href="/">Back home</a>
+          </div>
+          <Footer/></div>)
+      } else {
+        return(
+          <div>
+            <Header/><Sidemenu/>
+            <div className="account-dashboard">
+              <header>
+                <img src={avatar}/> <p>{this.state.user.username}</p>
+              </header>
+              <form onSubmit={this.formSubmit} onChange={this.onEnabled} className="container">
+                <p className="form-modal"></p>
+                <div className="form-group">
+                  <label>Email</label>
+                  <input name="email" className="form-control" type="email" value={this.state.user.email} required readonly/>
+                </div>
+                <div className="form-group">
+                  <label>Password</label>
+                  <input name="password" className="form-control" type="password" required/>
+                </div>
+                <hr/>
+                <div className="form-group">
+                  <label>User name</label>
+                  <input name="newUsername" className="form-control" defaultValue={this.state.user.username}/>
+                </div>
+                <div className="form-group">
+                  <label>Bio</label>
+                  <textarea name="bio" className="form-control" defaultValue={this.state.user.bio}></textarea>
+                </div>
+
+                <button className="btn btn-info" type="submit" disabled>Update</button>
+              </form>
+            </div>
+            <Footer/>
+          </div>
+        )
+      }
     }
   }
-}
 };
 
 export default Me;
