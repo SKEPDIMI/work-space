ea98a3b7fc4c4e7e35c35364e921e8c306d28af9
John S <diaz.john312@gmail.com>
2018-09-29 22:07:31 -0400

    set user default

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import reducers from './redux/reducers';
 
 import Routes from './components/util/Routes';
 
-import { fetchUser, setUser } from './redux/actions';
+import { fetchUser, setUserDefault } from './redux/actions';
 
 var store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -17,10 +17,13 @@ class App extends Component {
     );
 
     if (workspaceToken) { // If there is a JWT in our local storage we want to call the fetchUser action creator, and dispatch whatever it returns to us (an action to set store.user to the usersdata, fetched from the API)
-      store.dispatch(fetchUser())
-    } else { // If there is no JWT in out local storage, just set the user to false for user components to know that the user is undefined and not pending
-      this.setState({ done: true });
-      store.dispatch(setUser(false));
+      store.dispatch(
+        fetchUser(workspaceToken)
+      )
+    } else {
+      store.dispatch(
+        setUserDefault()
+      )
     }
   }
   render(){
