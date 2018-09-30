015430675f9448ab58256bd60bdef62347290eb1
John S <diaz.john312@gmail.com>
2018-09-29 22:07:15 -0400

    changed url to api

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 let apiURL = process.env.NODE_ENV === 'production' ?
-'https://workspace-api.herokuapp.com':'http://localhost:5000';
+'https://workspace-api.herokuapp.com/api':'http://localhost:5000/api';
 
 export default {
   apiURL : apiURL
